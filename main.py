75c2f8fb382057a14066f93a99e6106bf8fcfd0d
Will Farrell <farr1617@yahoo.com>
2019-02-03 22:49:07 -0800

    Done

--- main.py
+++ main.py
@@ -4,44 +4,45 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-#Define blank field to help assist with username field
-def is_blank(str):
-    if str == []:
-     return True
+@app.route("/")
+def index():
+    return render_template('homepage.html')
 
-#Create app route for signup form, with method rqmt, then define validation for each field in the
-#requestform(username, password, password confirmation and email)
 @app.route("/signup", methods=['POST'])
 def validation():
-    username = request.form['username']
-    password = request.form['password']
-    password_conf = request.form['password_conf']
-    email = request.form['email']
-    username_error = ''
-    password_error = ''
-    password_conf_error = ''
-    email_error = ''
-    
-#Add conditions for each field to standardize user inputs
-    if is_blank(username):
-        username_error = 'Not a valid username'
-        username = ''
-    if len(username) > 20 or len(username) < 3:
-            username_error = 'Username out of range (3-20 characters)'
-            username = ''
 
-    if is_blank(password and password_conf):
-        password_error = 'Please enter a password'
-        password = ''
-    if len(password and password_conf) > 20 or len(password and password_conf) < 3:
-            password_error = 'Password out of range (3-20 characters)'
-            password = ''
-    if password_conf != password:
-            password_conf_error = 'Password Must match!'
-            password_conf = ''
+    username = request.form["username"]
+    password = request.form["password"]
+    password_conf = request.form["password_conf"]
+    email = request.form["email"]
+    
+    username_error = ""
+    password_error = ""
+    password_conf_error = ""
+    email_error = ""
+
+    if username == "":
+        username_error = "Not a valid username"
+        username = ""
+    elif len(username) <= 3 or len(username) > 20:
+        username_error = 'Username out of range (3-20 characters)'
+        username = ''
+    elif " " in username:
+        username_error = "Username cannot contain any spaces"
+        username = ""
         
-           
-    if email:
+    if password == "": 
+        password_error = "Not a valid password"
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password must be between 3 and 20 characters long"
+    elif " " in password:
+        password_error = "Password cannot contain any spaces"
+       
+    if password_conf == "" or password_conf != password:
+        password_conf_error = "Password Must match!"
+        password_conf = ""
+    
+    if email != "":
         if email.count("@") < 1 or email.count("@") > 1:
             email_error = 'Please enter a valid email!'
         if email.count(".") < 1 or email.count(".") > 1:
@@ -51,9 +52,9 @@ def validation():
         if len(email) < 3 or len(email) > 20:
             email_error = "Email length out of range(3-20)"
     if not username_error and not password_error and not password_conf_error and not email_error:
-        return redirect('/welcome?username={0}'.format(username))
+        return render_template('welcomepage.html', user=username)                                
     else:
-        return render_template('homepage.html', 
+        return render_template('homepage.html',
                                 username_error=username_error,
                                 password_error=password_error,
                                 password_conf_error=password_conf_error,
@@ -66,8 +67,6 @@ def welcome():
     username = request.args.get('username')
     return render_template('welcomepage.html', user=username)                                
 
-@app.route("/")
-def index():
-    return render_template('homepage.html')
+
 
 app.run()
